1ce1bf01f85a0d7bad31b8495c92487643c512dd
preethi101 <52034801+preethi101@users.noreply.github.com>
2019-06-21 13:13:56 +0530

    Add files via upload

---
+++ Task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+//Task 1
+int main()
+{
+    char my_var;
+    cout<<"Please enter a character\n";
+    cin>>my_var;
+
+    int ascii_my_var;
+    ascii_my_var=my_var;
+    cout<<"\nThe ASCII value of your character is\t"<<ascii_my_var;
+
+    return 0;
+}
